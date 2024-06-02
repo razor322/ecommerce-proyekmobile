4f57f03327793bca046ddc53ead367dab399f49a
gybran nauval <nauvalgybran@gmail.com>
2024-06-02 22:18:33 +0700

    initial state

---
+++ lib/screen/cart/cart_detail_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CartdetailPage extends StatefulWidget {
+  const CartdetailPage({super.key});
+
+  @override
+  State<CartdetailPage> createState() => _CartdetailPageState();
+}
+
+class _CartdetailPageState extends State<CartdetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
