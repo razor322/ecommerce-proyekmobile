3e8b42af61adb47ea2dd01ca49c8619de39aaf32
gybran nauval <nauvalgybran@gmail.com>
2024-06-06 12:07:10 +0700

    minor changes

--- lib/const.dart
+++ lib/const.dart
@@ -1,5 +1,6 @@
 // const url = "http://192.168.100.54/project_ecommerce/api/";
 // const url = "http://10.127.238.137/project_ecommerce/api/";
-const ipaddr = "192.168.100.54";
+// const ipaddr = "192.168.100.54";
+const ipaddr = "10.126.229.226";
 const url = "http://$ipaddr/project_ecommerce/api/";
 const urlImg = "http://$ipaddr/project_ecommerce/product_image/";
